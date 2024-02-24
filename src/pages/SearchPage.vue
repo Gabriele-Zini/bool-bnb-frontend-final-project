950658d8f6f7a312e581c7c581e7eebe003f118e
andrearicciotti <ricciotti.andrea1993@gmail.com>
2024-02-24 10:31:17 +0100

    add: router.js file

---
+++ src/pages/SearchPage.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    
+}
+</script>
+
+<template>
+    <div>
+        
+    </div>
+
+</template>
+<style lang="scss" scoped>
+    
+</style>
---
+++ src/router.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomePage from './pages/HomePage.vue'
+import SearchPage from './pages/SearchPage.vue'
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+
+        {
+            path: "/",
+            name: "home",
+            component: HomePage,
+        },
+
+        {
+            path: "/",
+            name: "search",
+            component: SearchPage,
+        },
+    ]
+})
+
+export { router }
