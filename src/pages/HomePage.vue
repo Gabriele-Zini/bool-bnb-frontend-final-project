48f62cdead52c2e85d72b18fd9ce4676b14f3f41
Gabriele-Zini <139758244+Gabriele-Zini@users.noreply.github.com>
2024-02-26 19:42:28 +0100

    add: sponsorship apartments grid wip

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,39 +1,95 @@
 <script>
-import axios from 'axios';
-import { store } from '../store';
+import axios from "axios";
+import { store } from "../store";
 
 export default {
-    data() {
-        return {
-            store,
-        }
-        
-    }
-}
+  data() {
+    return {
+      store,
+      apartments: [],
+    };
+  },
+
+  created() {
+    this.fetchData();
+  },
+  methods: {
+    fetchData() {
+      axios.get(`${this.store.baseUrl}/api/apartments`).then((resp) => {
+        console.log(resp.data);
+        this.apartments = resp.data.result.data;
+      });
+    },
+  },
+};
 </script>
 <template>
-<div class="container-fluid">
+  <div class="container-fluid">
     <div class="title m-5">
-        <h1>BOOLBNB</h1>
+      <h1>BOOLBNB</h1>
     </div>
-</div>
+  </div>
 
+  <!-- apartment--card -->
+  <div class="ms_container">
+    <div class="row justify-content-center my-5">
+      <div class="col-md-10">
+        <div class="row">
+          <div
+            class="col-12 col-md-6 col-lg-3 mb-4"
+            v-for="apartment in apartments"
+            :key="apartment.id"
+          >
+            <div class="card" style="height: 30rem">
+              <img
+                :src="`${store.baseUrl}/storage/image_path/${apartment.image_path}`"
+                alt=""
+                class="card-img-top"
+              />
+              <!-- <div class="card-body"> -->
+              <h5 class="card-title mt-2 fs-6">{{ apartment.title }}</h5>
+              <p class="m-0 p-0">
+                {{ apartment.street_name }} {{ apartment.street_number }}
+              </p>
+              <p class="m-0 p-0">{{ apartment.city }}</p>
+
+              <a
+                href="#"
+                class="btnn"
+                data-bs-toggle="modal"
+                data-bs-target="#exampleModal"
+                @click="message(apartment.slug)"
+                >Send a message</a
+              >
+              <router-link
+                :to="{ name: 'apartmentInfo', params: { slug: apartment.slug } }"
+                class="rem"
+                >Mostra</router-link
+              >
+              <!-- </div> -->
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- /apartment--card -->
 </template>
 <style lang="scss" scoped>
 .container-fluid {
-    background-image: url(../assets/img/interior-home.jpg);
-    background-size: cover;
-    position: absolute;
-    background-position: center;
-    height: 500px;
+  background-image: url(../assets/img/interior-home.jpg);
+  background-size: cover;
+  position: absolute;
+  background-position: center;
+  height: 500px;
 
-    .title {
-        display: flex;
-        justify-content: center;
-        h1 {
-            font-size: 5rem;
-            color: white;
-        }
+  .title {
+    display: flex;
+    justify-content: center;
+    h1 {
+      font-size: 5rem;
+      color: white;
     }
+  }
 }
 </style>
