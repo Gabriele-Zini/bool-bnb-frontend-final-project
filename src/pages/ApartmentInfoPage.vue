3587a6c585e47ae7cb6d38cc9508f4f639dee2c6
Gabriele-Zini <139758244+Gabriele-Zini@users.noreply.github.com>
2024-02-28 23:04:57 +0100

    api views

--- src/pages/ApartmentInfoPage.vue
+++ src/pages/ApartmentInfoPage.vue
@@ -8,7 +8,8 @@ export default {
             store,
             apartment: {},
             loading: false,
-
+            ip: null,
+            apartment_id:undefined,
         };
     },
     created() {
@@ -16,13 +17,23 @@ export default {
             console.log(resp.data.result);
             this.loading = true;
             this.apartment = resp.data.result;
+            this.apartment_id=resp.data.result.id
+            
+            axios.get('https://api.db-ip.com/v2/free/self').then(resp => {
+                // console.log(resp.data.ipAddress)
+                this.ip = resp.data.ipAddress;
+                console.log(this.apartment_id)
+                console.log(this.ip)
+                const params={
+                    user_ip:this.ip,
+                    apartment_id: this.apartment_id
+                }
+                axios.post(`${this.store.baseUrl}/api/get-view`, params )
         })
-        axios.get('https://api.db-ip.com/v2/free/self').then(resp=> {
-           console.log(resp.data.ipAddress)
         })
-        axios.get('https://api.ipify.org?format=json').then(resp=> {
-           console.log(resp)
-        })
+        /* axios.get('https://api.ipify.org?format=json').then(resp => {
+            console.log(resp.data.ip)
+        }) */
     }
 
 }
@@ -33,12 +44,12 @@ export default {
             <div class="mx-auto" v-if="loading">
                 <h2 class="mb-5 text-center"> {{ apartment.title }} </h2>
                 <div class="cardss d-flex flex-wrap justify-content-center gap-2 border-bottom pb-5 rounded">
-                    <div  v-for="image in apartment.images">
+                    <div v-for="image in apartment.images">
                         <img class="rounded" v-if="apartment.images.length === 0" :src="getImage('no_Image_Available.jpg')"
                             :alt="apartment.title">
                         <div v-else class="">
                             <div class="">
-                                <img  :src="`${store.baseUrl}/storage/image_path/${image.image_path}`" class="border rounded"
+                                <img :src="`${store.baseUrl}/storage/image_path/${image.image_path}`" class="border rounded"
                                     :alt="apartment.title">
                             </div>
                         </div>
@@ -48,30 +59,38 @@ export default {
                     <div class="bro">
                         <h4 class="text-center pt-3">Caratterische</h4>
                         <ul class="list">
-                            <li><strong><i class="fa-solid fa-house"></i> Meters square:</strong> {{ apartment.apartment_info.mt_square }} </li>
-                            <li><i class="fa-solid fa-bath"></i> <strong>Bathrooms:</strong> {{ apartment.apartment_info.num_bathrooms }}</li>
-                            <li><i class="fa-solid fa-bed"></i> <strong>Beds:</strong> {{ apartment.apartment_info.num_beds }} </li>
-                            <li><i class="fa-solid fa-hotel"></i> <strong>Rooms:</strong> {{ apartment.apartment_info.num_rooms }}</li>
-                            <li><i class="fa-solid fa-road"></i> <strong>Is located in:</strong> {{ apartment.street_name }} n. {{ apartment.street_number }}, {{
-                        apartment.postal_code }}, {{ apartment.city }} ({{ apartment.country }})</li>
+                            <li><strong><i class="fa-solid fa-house"></i> Meters square:</strong> {{
+                                apartment.apartment_info.mt_square }} </li>
+                            <li><i class="fa-solid fa-bath"></i> <strong>Bathrooms:</strong> {{
+                                apartment.apartment_info.num_bathrooms }}</li>
+                            <li><i class="fa-solid fa-bed"></i> <strong>Beds:</strong> {{ apartment.apartment_info.num_beds
+                            }} </li>
+                            <li><i class="fa-solid fa-hotel"></i> <strong>Rooms:</strong> {{
+                                apartment.apartment_info.num_rooms }}</li>
+                            <li><i class="fa-solid fa-road"></i> <strong>Is located in:</strong> {{ apartment.street_name }}
+                                n. {{ apartment.street_number }}, {{
+                                    apartment.postal_code }}, {{ apartment.city }} ({{ apartment.country }})</li>
                             <li><strong>Services: </strong>
-                    <p class="d-inline" v-for="(service, index) in apartment.services" :key="service.index">{{ index > 0 ? ', ' : '' }}{{ service.name }}{{ index == apartment.services.length - 1 ? '.' : '' }}</p></li>
+                                <p class="d-inline" v-for="(service, index) in apartment.services" :key="service.index">{{
+                                    index > 0 ? ', ' : '' }}{{ service.name }}{{ index == apartment.services.length - 1 ?
+        '.' : '' }}</p>
+                            </li>
                         </ul>
-                    <!-- <strong>Meters square:</strong> {{ apartment.apartment_info.mt_square }} <br>
+                        <!-- <strong>Meters square:</strong> {{ apartment.apartment_info.mt_square }} <br>
                     <strong>Bathrooms:</strong> {{ apartment.apartment_info.num_bathrooms }} <br>
                     <strong>Beds:</strong> {{ apartment.apartment_info.num_beds }} <br>
                     <strong>Rooms:</strong> {{ apartment.apartment_info.num_rooms }} <br> -->
 
-                    <!-- <strong>Is located in:</strong> {{ apartment.street_name }} n. {{ apartment.street_number }}, {{
+                        <!-- <strong>Is located in:</strong> {{ apartment.street_name }} n. {{ apartment.street_number }}, {{
                         apartment.postal_code }}, {{ apartment.city }} ({{ apartment.country }}) <br>
                     <strong>Services: </strong>
                     <p class="d-inline" v-for="(service, index) in apartment.services" :key="service.index">{{ index > 0 ? ', ' : '' }}{{ service.name }}{{ index == apartment.services.length - 1 ? '.' : '' }}</p> -->
+                    </div>
+
+                    <div>
+                    </div>
                 </div>
 
-                <div>
-                </div>
-            </div>
-            
             </div>
 
             <!-- <div v-else class="col-6 mx-auto spinner-border" role="status">
@@ -86,10 +105,11 @@ export default {
 @use "../style/partials/variables" as *;
 
 .bro {
-  background-color: #F2F4F7;
-  border: 1px solid #EAECF0;
-  border-radius: 10px
+    background-color: #F2F4F7;
+    border: 1px solid #EAECF0;
+    border-radius: 10px
 }
+
 .list {
     list-style-type: none;
 }
@@ -98,11 +118,11 @@ img {
     height: 350px;
     aspect-ratio: 1/1;
 }
+
 .cards {
     border-radius: 10px;
     margin: 10px;
     padding: 10px;
     line-height: 3rem;
 }
-
 </style>
