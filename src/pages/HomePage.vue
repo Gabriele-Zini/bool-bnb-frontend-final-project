2f06da69416e0f3123d65cd080a75abdb1f51678
Gabriele-Zini <139758244+Gabriele-Zini@users.noreply.github.com>
2024-02-29 12:09:01 +0100

    add:  target="_blank" to details route

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -74,7 +74,7 @@ export default {
 
 
                     <router-link :to="{ name: 'apartmentInfo', params: { slug: apartment.slug } }"
-                      class="my_btn_primary">Mostra</router-link>
+                      class="my_btn_primary"  target="_blank">Mostra</router-link>
                 </div>
               <!-- </div> -->
             </div>
