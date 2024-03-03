71c51f4b7e690158805ba33a94e5699fa0760038
andrearicciotti <ricciotti.andrea1993@gmail.com>
2024-03-04 00:37:31 +0100

    add: 404 page

--- src/pages/NotFoundPage.vue
+++ src/pages/NotFoundPage.vue
@@ -1,15 +1,45 @@
 <script>
 export default {
-    
+
 }
 </script>
 
 <template>
-    <div>
-        
+    <div class="mt-5 w-100">
+        <div class="ms_video-container">
+            <video autoplay loop muted src="../assets/video/gobackhome.mp4" class="w-100 ms_404-video"></video>
+            <h2 class="fs-2">You're way off track</h2>
+            <router-link :to="{ name: 'home' }" class="ms_link nav-link fs-3 me-2">Go back <i class="fa-solid fa-house ms-1">Home</i>
+            </router-link>
+
+        </div>
     </div>
 </template>
 
-<style lang="" scoped>
-    
+<style lang="scss" scoped>
+
+.ms_video-container {
+    position: relative;
+    display: flex;
+    justify-content: center;
+
+    .ms_404-video {
+        filter: brightness(0.60);
+    }
+
+    h2 {
+        position: absolute;
+        bottom: 105px;
+        color: #f2f4f7;
+        filter: drop-shadow(10px 10px 10px black);
+        // font-size: 2rem;
+    }
+
+    .ms_link {
+        position: absolute;
+        bottom: 55px;
+        color: #f2f4f7;
+        filter: drop-shadow(10px 10px 10px black);
+    }
+}
 </style>
